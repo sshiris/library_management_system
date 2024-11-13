fb1bfa78137e87cb7106719145363b874c42dec3
Iris <148431646+iris915520@users.noreply.github.com>
2024-11-13 16:28:01 +0200

    add LibraryItem abstract class

---
+++ src/LibraryItem.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+
+abstract class LibraryItem {
+    private int id;
+    protected boolean availability;
+    protected LocalDate dueToDate;
+
+    public LibraryItem(int id) {
+        this.id = id;
+        this.availability = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAvailable() {
+        return availability;
+    }
+
+    public abstract void borrowItem();
+
+    public void returnItem() {
+        availability = true;
+        dueToDate = null;
+        System.out.println("Item widh ID: " + id + " has been returned and now it is available!");
+    }
+
+}
