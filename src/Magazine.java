c5194de130403225f153570c9a7568422b167cfa
Iris <148431646+iris915520@users.noreply.github.com>
2024-11-13 21:03:07 +0200

    add Magazine class extends LibraryItem

---
+++ src/Magazine.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+class Magazine extends LibraryItem {
+    private String issue;
+
+    public Magazine(String issue, int id) {
+        super(id);
+        this.issue = issue;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void borrowItem() {
+        if (availability) {
+            this.availability = false;
+            LocalDate borrowDate = LocalDate.now();
+            this.dueToDate = borrowDate.plusWeeks(2);
+            System.out.println("The magazine " + issue + "  has been borrowed, the due date is " + dueToDate);
+        } else {
+            System.out.println("The magazine " + issue + " is not available");
+        }
+    }
+}
