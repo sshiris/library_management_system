ac476fe430a27c1f47622602f9cfa36069e777da
Iris <148431646+iris915520@users.noreply.github.com>
2024-11-19 22:08:17 +0200

    add LibraryManager

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -1,5 +1,36 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryManager {
+    private List<LibraryItem> libraryItems;
+
+    public LibraryManager() {
+        libraryItems = new ArrayList<>();
     }
+
+    public void addLibraryItem(LibraryItem item) {
+        libraryItems.add(item);
+        System.out.println("Library item is added: " + item.getId());
+    }
+
+    public void removeLibraryItem(LibraryItem item) {
+        if (libraryItems.remove(item)) {
+            System.out.println("Library item removed: " + item.getId());
+        } else {
+            System.out.println("Library item not found");
+        }
+        ;
+    }
+
+    public void display() {
+        for (LibraryItem item : libraryItems) {
+            String status = item.availability ? " is available" : " is borrowed, the return date is " + item.dueToDate;
+            if (item instanceof Book) {
+                System.out.println("Book " + item.getId() + " : " + ((Book) item).getTitle() + status);
+            } else {
+                System.out.println("Magzine " + item.getId() + " : " + ((Magazine) item).getIssue() + status);
+            }
+        }
+    }
+
 }
